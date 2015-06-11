f9e22d705f7d459f8df0203686f9b7e2d7ff031f
Julia <schwarzkruppzo@gmail.com>
2015-06-11 19:17:06 +0400

    Added load library for easy-peasy-like-squeasy

--- lua/autorun/load.lua
+++ lua/autorun/load.lua
@@ -1,18 +1,37 @@
-if SERVER then
-	AddCSLuaFile()
-	AddCSLuaFile("external/sh_netstream2.lua")
-	AddCSLuaFile("external/sh_pon.lua")
-	AddCSLuaFile("meta/container.lua")
-	AddCSLuaFile("meta/item.lua")
-	AddCSLuaFile("sh_inv.lua")
-	
-	include("external/sv_database.lua")
+if SERVER then AddCSLuaFile() end
+
+load = {}
+
+function load.Include( path, side )
+	if !path then
+		return
+	end
+	if ( side == "server" or path:find( "sv_" ) ) and SERVER then
+		include( path )
+	elseif side == "shared" or path:find( "sh_" ) then
+		if (SERVER) then
+			AddCSLuaFile( path )
+		end
+		include(path)
+	elseif side == "client" or path:find( "cl_" ) then
+		if SERVER then
+			AddCSLuaFile( path )
+		else
+			include( path )
+		end
+	end
 end
-include("external/sh_netstream2.lua")
-include("external/sh_pon.lua")
-include("meta/container.lua")
-include("meta/item.lua")
-include("sh_inv.lua")
+function load.IncludeDir( dir )
+	local gamemodeDir = "abdul_bomz/gamemode/" // for future using
+
+	for k, v in ipairs( file.Find( dir.."/*.lua", "LUA") ) do
+		load.Include( dir.."/"..v )
+	end
+end
+
+load.IncludeDir( "external" )
+load.IncludeDir( "meta" )
+load.Include( "sh_inv.lua" )
 
 inv.LoadFromDirectory( "classes/containers", false )
 inv.LoadFromDirectory( "classes/items", true )
