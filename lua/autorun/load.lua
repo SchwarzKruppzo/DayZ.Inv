adbf0af5335373b1c2acf4bb057f092977c4dde9
SchwarzKruppzo <schwarzkruppzo@gmail.com>
2016-02-14 17:44:41 +0300

    Fixed file loading; Moved database connection to this file.

--- lua/autorun/load.lua
+++ lua/autorun/load.lua
@@ -1,19 +1,20 @@
 if SERVER then AddCSLuaFile() end
 
 load = {}
+invmeta = {}
 
-function load.Include( path, side )
+function load.Include( path )
 	if !path then
 		return
 	end
-	if ( side == "server" or path:find( "sv_" ) ) and SERVER then
+	if path:find( "sv_" ) and SERVER then
 		include( path )
-	elseif side == "shared" or path:find( "sh_" ) then
+	elseif path:find( "sh_" ) then
 		if (SERVER) then
 			AddCSLuaFile( path )
 		end
 		include(path)
-	elseif side == "client" or path:find( "cl_" ) then
+	elseif path:find( "cl_" ) then
 		if SERVER then
 			AddCSLuaFile( path )
 		else
@@ -29,9 +30,20 @@ function load.IncludeDir( dir )
 	end
 end
 
-load.IncludeDir( "external" )
-load.IncludeDir( "meta" )
+load.Include( "external/sh_netstream2.lua" )
+load.Include( "external/sh_pon.lua" )
+load.Include( "external/sv_database.lua" )
+load.Include( "meta/sh_container.lua" )
+load.Include( "meta/sh_item.lua" )
 load.Include( "sh_inv.lua" )
 
 inv.LoadFromDirectory( "classes/containers", false )
 inv.LoadFromDirectory( "classes/items", true )
+
+if SERVER then
+	database.Connect(function()
+		database.LoadTables()
+			
+		MsgC( Color(0, 255, 0), "DayZ.Inv has connected to the database.\n" )
+	end)
+end
